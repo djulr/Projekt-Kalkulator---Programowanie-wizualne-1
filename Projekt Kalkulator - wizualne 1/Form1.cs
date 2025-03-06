10929394a56ba6753415827a71d74c7c9f0cc6c2
rocha <rocha@JULIA>
2025-03-06 14:53:17 +0100

    Dodaj pliki projektów.

---
+++ Projekt Kalkulator - wizualne 1/Form1.cs
@@ -0,0 +1,45 @@
+namespace Projekt_Kalkulator___wizualne_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
